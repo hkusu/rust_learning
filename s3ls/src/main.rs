c85c727d232edd279294aa71839e0df54f651614
Hiroyuki Kusu <kusu0806@gmail.com>
2022-06-24 00:31:18 +0900

    Add sample to access S3

--- s3ls/src/main.rs
+++ s3ls/src/main.rs
@@ -1,13 +1,35 @@
-use std::path::PathBuf;
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_s3::{Client, Error, Region};
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(Debug, StructOpt)]
 struct Opt {
-    #[structopt(name = "BUCKET")]
+    #[structopt(short)]
+    region: Option<String>,
+
     bucket: String,
 }
 
-fn main() {
-    let opt = Opt::from_args();
-    println!("{}", opt.bucket);
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+
+    let Opt { region, bucket } = Opt::from_args();
+
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("ap-northeast-1"));
+
+    // println!("{}", region_provider.region().await.unwrap().as_ref());
+
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+
+    let client = Client::new(&shared_config);
+
+    let resp = client.list_objects_v2().bucket(bucket).send().await?;
+
+    for object in resp.contents().unwrap_or_default() {
+        println!("{}", object.key().unwrap_or_default());
+    }
+
+    Ok(())
 }
